6751c8ef78730c05347b958b79819e50001358bb
Nurlan Salahaddinov <nurlan.salahaddinov@oracle.com>
2022-03-22 19:37:30 +0100

    Models correction. Not tested yet

--- users/models.py
+++ users/models.py
@@ -13,11 +13,33 @@ class UserBase(models.Model):
     def getInitials(self):
         return f'{self.name[0].lower() + self.surname.lower()}'
 
+class UserLocale(models.Model):
+    userbase = models.ForeignKey("app.UserBase", on_delete=models.CASCADE)
+    country = models.CharField("country", max_length=20)
+    city = models.CharField("city", max_length=20)
+    state = models.CharField("state", max_length=20)
+    postcode = models.CharField("postcode", max_length=20)
+    street = models.CharField("street", max_length=50)
+
+    def __str__(self):
+        return f'{userbase} {street}'
+
+class UserContactData(models.Model):
+    userbase = models.ForeignKey("app.UserBase", on_delete=models.CASCADE)
+    email = models.CharField("email", max_length=50)
+    cell = models.CharField("", max_length=20)
+    picture = models.BinaryField("picture")
+
+    def __str__(self):
+        return f'{email} {cell}'
 
 class Weather(models.Model):
+    userbase = models.ForeignKey("app.UserBase", on_delete=models.CASCADE)
     city = models.CharField("city", max_length=30)
     country = models.CharField("country", max_length=30)
-    weather_celcius = models.IntegerField("weather_celsius", default=0)
+    celcius = models.IntegerField("celsius", default=0)
+    fahrenheit = models.IntegerField("fahrenheit", default=0)
+    wind = models.IntegerField("wind m/s", default=0)
 
     def __str__(self):
         return f'{city} {weather_celcius}'
